7fcb99c9d5a3275871dd2fa9565582e76f9aa6e6
unknown <simonas.b@hotmail.com>
2025-08-20 21:13:38 +0300

    init repo, add migration for posts, model, controller. add laravel/sail docker compose

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index() 
+    {
+        return Post::all();
+    }
+
+    public function show($id) 
+    {
+        return Post::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        Post::create($request->all());
+
+        return response()->json("new post has been created", 201);
+    }
+
+    public function edit($id)
+    {
+        return Post::find($id);
+    }
+
+    public function update(Request $request) 
+    {
+        $post = Post::find($id);
+        if (!$post) {
+            return response()->json(['message' => 'post not found'], 404);
+        }
+
+        $post->update($request->all());
+        return response()->json("post has been updated", 201);
+    }
+
+    public function destroy($id)
+    {
+        Post::find($id)->delete();
+
+        return response()->json("post has been deleted", 201);
+    }
+}
